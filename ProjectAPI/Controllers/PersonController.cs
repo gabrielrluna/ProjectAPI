58af53ab6c5c493a7cecb4fcc47a9b2f85402f23
Gabriel Luna <lunagabri@gmail.com>
2023-05-05 09:55:13 -0300

    Complemento

--- ProjectAPI/Controllers/PersonController.cs
+++ ProjectAPI/Controllers/PersonController.cs
@@ -17,13 +17,59 @@ namespace ProjectAPI.Controllers
         }
         //get //post //put //patch  //delete
 
-        [HttpGet ("")]
-        public IActionResult Get() {
-            var novo = new PersonModel(1, "");
-            return Ok(novo); //200
-            //201
-            //400
-            //500
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<PersonModel> person = new List<PersonModel>();
+            person.Add(new PersonModel(1, "José"));
+            person.Add(new PersonModel(2, "Maria"));
+            person.Add(new PersonModel(3, "Luiz"));
+            person.Add(new PersonModel(4, "Cláudia"));
+            person.Add(new PersonModel(5, "Marcos"));
+
+            return Ok(person); //200
+
         }
+
+        [HttpDelete()]
+        public IActionResult Delete([FromQuery] int id)
+        {
+            if (id == 0)
+                return BadRequest("Opa! Você não pode usar este ID");
+            // Rota para deletar
+            return Ok(); //200
+
+        }
+
+        [HttpPost()]
+        public IActionResult Create([FromBody] PersonModel model)
+        {
+
+            model.Id = 10;
+            //return Created("Get/", model.Id); //201
+            return StatusCode(StatusCodes.Status201Created, model.Id);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] PersonModel model)
+        {
+            if (id == 0)
+            {
+                return BadRequest("Opa! Você não pode usar este ID");
+            }
+            return Ok();
+        }
+
+
+        [HttpPatch("{id:int}")]
+        public IActionResult Update2([FromRoute] int id, [FromBody] PersonModel model)
+        {
+            if (id == 0)
+            {
+                return BadRequest("Opa! Você não pode usar este ID");
+            }
+            return Ok();
+        }
+
     }
 }
